d8a749e2ca8f45424d8573d41e8989d8cd50ba83
Paul Ambrose <pablo_rose34@hotmail.com>
2024-04-14 18:26:42 +0100

    finished initial implementation of the player object in the game, populating the oxygen level and suit integrity

--- RPG_v2_refactor/spaceAdventure.cs
+++ RPG_v2_refactor/spaceAdventure.cs
@@ -13,10 +13,19 @@ namespace RPG_v2_refactor
 {
     public partial class spaceAdventure : Form
     {
-        private Player _player;
+        private Player _PlayerNumber1;
         public spaceAdventure()
         {
             InitializeComponent();
+            _PlayerNumber1 = new Player();
+
+            _PlayerNumber1.OxygenLevel = 100;
+            _PlayerNumber1.suitIntegrity = 100;
+            _PlayerNumber1.inFight = false;
+            _PlayerNumber1.withEscapePod = false;
+
+            oxygenLevel.Text = _PlayerNumber1.OxygenLevel.ToString();
+            suitIntegrity.Text = _PlayerNumber1.suitIntegrity.ToString();
         }
     }
 }
