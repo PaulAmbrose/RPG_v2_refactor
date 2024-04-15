64167a85afd8530c055a415ddc49086e90aece58
Paul Ambrose <pablo_rose34@hotmail.com>
2024-04-15 20:02:53 +0100

    addition of room and monster classes to engine

---
+++ engine/Monster.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace engine
+{
+    internal class Monster
+    {
+        public int MonsterSuitIntegrity { get; set; }
+
+        public string RoomLocation { get; set; }
+    }
+}
---
+++ engine/Rooms.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace engine
+{
+    public class Rooms
+    {
+        public string RoomName { get; set; }
+        public bool isMonsterPresent { get; set; }
+        public bool isEscapePodPresent { get; set; }
+
+    }
+}
